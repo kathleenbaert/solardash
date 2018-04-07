1f81f4003f56f1557171ff760fd13ccc58f08267
Kathleen Baert <kathleen.baert@marquette.edu>
2018-04-07 14:55:20 -0500

    performed data valudation on the critical values

--- app/src/main/java/com/example/kathleenbaert/myapplication/CriticalValueFragment.java
+++ app/src/main/java/com/example/kathleenbaert/myapplication/CriticalValueFragment.java
@@ -1,7 +1,9 @@
 package com.example.kathleenbaert.myapplication;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -22,9 +24,10 @@ public class CriticalValueFragment extends Fragment {
     private final String TAG = "CriticalValueFragment";
     View myView;
     EditText editText;
-    Context context;
+    public Context context;
     static CriticalValues cv = new CriticalValues();
     private DrawerLayout drawerLayout;
+    public boolean saveTheseValues;
 
     @Nullable
     @Override
@@ -36,94 +39,170 @@ public class CriticalValueFragment extends Fragment {
         return myView;
 
     }
+
     private void textViews() {
         Button saveButton = (Button) myView.findViewById( R.id.save_button );
         saveButton.setOnClickListener( get_edit_view_button_listener );
 
     }
 
-    private void savePreferences(String key, String value) {
+    private void savePreferences(String key, int value) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences( context );
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString( key, value );
+        editor.putInt( key, value );
         editor.commit();
     }
 
     private void loadSavedPreferences() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences( context );
         editText = myView.findViewById( R.id.speed_warning );
-        String value = sharedPreferences.getString( "speedWarning", "0" );
-        editText.setText( value );
+        int value = sharedPreferences.getInt( "speedWarning", 0 );
+        editText.setText( value + "" );
 
         editText = myView.findViewById( R.id.speed_critical );
-        value = sharedPreferences.getString( "speedCritical", "0" );
-        editText.setText( value );
+        value = sharedPreferences.getInt( "speedCritical", 0 );
+        editText.setText( value + "" );
 
         editText = myView.findViewById( R.id.temp_warning );
-        value = sharedPreferences.getString( "tempWarning", "0" );
-        editText.setText( value );
+        value = sharedPreferences.getInt( "tempWarning", 0 );
+        editText.setText( value + "" );
 
         editText = myView.findViewById( R.id.temp_critical );
-        value = sharedPreferences.getString( "tempCritical", "0" );
-        editText.setText( value );
+        value = sharedPreferences.getInt( "tempCritical", 0 );
+        editText.setText( value + "" );
 
         editText = myView.findViewById( R.id.battery_warning );
-        value = sharedPreferences.getString( "batteryWarning", "0" );
-        editText.setText( value );
+        value = sharedPreferences.getInt( "batteryWarning", 0 );
+        editText.setText( value + "" );
 
         editText = myView.findViewById( R.id.battery_critical );
-        value = sharedPreferences.getString( "batteryCritical", "0" );
-        editText.setText( value );
+        value = sharedPreferences.getInt( "batteryCritical", 0 );
+        editText.setText( value + "" );
 
         editText = myView.findViewById( R.id.voltage_warning );
-        value = sharedPreferences.getString( "voltageWarning", "0" );
-        editText.setText( value );
+        value = sharedPreferences.getInt( "voltageWarning", 0 );
+        editText.setText( value + "" );
 
         editText = myView.findViewById( R.id.voltage_critical );
-        value = sharedPreferences.getString( "voltageCritical", "0" );
-        editText.setText( value );
+        value = sharedPreferences.getInt( "voltageCritical", 0 );
+        editText.setText( value + "" );
     }
 
     private Button.OnClickListener get_edit_view_button_listener = new Button.OnClickListener() {
         public void onClick(View v) {
-            editText = myView.findViewById( R.id.speed_warning );
-            cv.setSpeedWarning( Integer.parseInt( editText.getText().toString() ) );
-            savePreferences( "speedWarning", editText.getText().toString() );
+            saveTheseValues = true;
+            //parse out the ints
+            int speedWarning = 0, speedCritical = 0, tempWarning = 0, tempCritical = 0, batteryWarning = 0, batteryCritical = 0, voltageWarning = 0, voltageCritical = 0;
+            try {
+                editText = myView.findViewById( R.id.speed_warning );
+                speedWarning = Integer.parseInt( editText.getText().toString() );
 
-            editText = myView.findViewById( R.id.speed_critical );
-            cv.setSpeedCritical( Integer.parseInt( editText.getText().toString() ) );
-            savePreferences( "speedCritical", editText.getText().toString() );
+                editText = myView.findViewById( R.id.speed_critical );
+                speedCritical = Integer.parseInt( editText.getText().toString() );
 
-            editText = myView.findViewById( R.id.temp_warning );
-            cv.setTempWarning( Integer.parseInt( editText.getText().toString() ) );
-            savePreferences( "tempWarning", editText.getText().toString() );
+                editText = myView.findViewById( R.id.temp_warning );
+                tempWarning = Integer.parseInt( editText.getText().toString() );
 
-            editText = myView.findViewById( R.id.temp_critical );
-            cv.setTempCritical( Integer.parseInt( editText.getText().toString() ) );
-            savePreferences( "tempCritical", editText.getText().toString() );
+                editText = myView.findViewById( R.id.temp_critical );
+                tempCritical = Integer.parseInt( editText.getText().toString() );
 
-            editText = myView.findViewById( R.id.battery_warning );
-            cv.setBatteryWarning( Integer.parseInt( editText.getText().toString() ) );
-            savePreferences( "batteryWarning", editText.getText().toString() );
+                editText = myView.findViewById( R.id.battery_warning );
+                batteryWarning = Integer.parseInt( editText.getText().toString() );
 
-            editText = myView.findViewById( R.id.battery_critical );
-            cv.setBatteryCritical( Integer.parseInt( editText.getText().toString() ) );
-            savePreferences( "batteryCritical", editText.getText().toString() );
+                editText = myView.findViewById( R.id.battery_critical );
+                batteryCritical = Integer.parseInt( editText.getText().toString() );
 
-            editText = myView.findViewById( R.id.voltage_warning );
-            cv.setVoltageWarning( Integer.parseInt( editText.getText().toString() ) );
-            savePreferences( "voltageWarning", editText.getText().toString() );
+                editText = myView.findViewById( R.id.voltage_warning );
+                voltageWarning = Integer.parseInt( editText.getText().toString() );
 
-            editText = myView.findViewById( R.id.voltage_critical );
-            cv.setVoltageCritical( Integer.parseInt( editText.getText().toString() ) );
-            savePreferences( "voltageCritical", editText.getText().toString() );
+                editText = myView.findViewById( R.id.voltage_critical );
+                voltageCritical = Integer.parseInt( editText.getText().toString() );
 
-            //works!
-            //System.out.println(cv.toString());
-            System.out.println("from critical values " + cv.toString());
-            Snackbar snackbar = Snackbar.make( myView, "Saved numbers", Snackbar.LENGTH_SHORT );
-            snackbar.show();
+            } catch (NumberFormatException e) {
+                alertBuilder( "Number Format Exception", "Please ensure you entered all 8 values, and there are no negatives or decimal numbers." );
+                saveTheseValues = false;
+            }
+
+            //set all the critical values
+            try {
+                cv.setSpeedWarning( speedWarning );
+                cv.setSpeedCritical( speedCritical );
+                cv.setTempWarning( tempWarning );
+                cv.setTempCritical( tempCritical );
+                cv.setBatteryWarning( batteryWarning );
+                cv.setBatteryCritical( batteryCritical );
+                cv.setVoltageWarning( voltageWarning );
+                cv.setVoltageCritical( voltageCritical );
+            } catch (Exception e) {
+                alertBuilder( "Problem occurred", "Problem has occurred while setting the critical values. Error: " + e.toString() );
+                saveTheseValues = false;
+            }
+            //savePreferences
+            try {
+                savePreferences( "speedWarning", speedWarning );
+
+                savePreferences( "speedCritical", speedCritical );
+
+                savePreferences( "tempWarning", tempWarning );
+
+                savePreferences( "tempCritical", tempCritical );
+
+                savePreferences( "batteryWarning", batteryWarning );
+
+                savePreferences( "batteryCritical", batteryCritical );
+
+                savePreferences( "voltageWarning", voltageWarning );
+
+                savePreferences( "voltageCritical", voltageCritical );
+            } catch (Exception e) {
+                alertBuilder( "Problem occurred", "Problem has occurred while saving your preferences in SharedPreferences. Error " + e.toString() );
+                saveTheseValues = false;
+            }
+            //data validation
+            if (speedWarning > speedCritical || speedWarning == speedCritical) {
+                alertBuilder( "Fix Speed Values", "Ensure Speed Critical Value is greater than Speed Warning Value, and they are not the same value." );
+                saveTheseValues = false;
+            }
+            if (tempWarning > tempCritical || tempWarning == tempCritical) {
+                alertBuilder( "Fix Temperature Values", "Ensure Temperature Critical Value is greater than Temperature Warning Value, and they are not the same value." );
+                saveTheseValues = false;
+            }
+            if (batteryWarning < batteryCritical || batteryCritical == batteryWarning) {
+                alertBuilder( "Fix Battery Values", "Ensure Battery Warning Value is greater than Battery Critical Value, and they are not the same value." );
+                saveTheseValues = false;
+            }
+            if (voltageWarning > voltageCritical || voltageWarning == voltageCritical) {
+                alertBuilder( "Fix Voltage Values", "Ensure Voltage Critical Value is greater than Voltage Warning Value, and they are not the same value." );
+                saveTheseValues = false;
+            }
+            if (batteryCritical > 100 || batteryWarning > 100) {
+                alertBuilder( "Fix Battery Values", "Ensure Battery Values are less than 100. Remember, this is a percentage." );
+                saveTheseValues = false;
+            }
+            if(speedWarning == 0 || speedCritical == 0 || tempWarning == 0 || tempCritical == 0 || batteryWarning == 0 || batteryCritical == 0 || voltageWarning == 0 || voltageCritical == 0){
+                alertBuilder("Zero Value", "Ensure none of the values are 0.");
+                saveTheseValues = false;
+            }
+
+            if (saveTheseValues) {
+                Snackbar snackbar = Snackbar.make( myView, "Saved numbers", Snackbar.LENGTH_SHORT );
+                snackbar.show();
+            }
         }
     };
+
+    public void alertBuilder(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder( context );
+        builder.setMessage( message ).setTitle( title );
+        builder.setPositiveButton( "OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        } );
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
 }
 
